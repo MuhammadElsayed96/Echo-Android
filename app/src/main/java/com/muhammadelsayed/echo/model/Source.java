403b1042367579fd98ac3740d55079b3c0e336e6
Muhammad Elsayed <muhammadelsayed96@yahoo.com>
2018-07-20 02:16:02 +0200

    Adding sources recyclerView

--- app/src/main/java/com/muhammadelsayed/echo/model/Source.java
+++ app/src/main/java/com/muhammadelsayed/echo/model/Source.java
@@ -3,6 +3,17 @@ package com.muhammadelsayed.echo.model;
 public class Source {
     private String id;
     private String name;
+    private String mSourceTitle;
+    private int mImageResourceID;
+    private boolean mToggleStatus;
+
+
+    public Source(String mSourceTitle, int mImageResourceID, boolean mToggleStatus) {
+        this.mSourceTitle = mSourceTitle;
+        this.mImageResourceID = mImageResourceID;
+        this.mToggleStatus = mToggleStatus;
+    }
+
 
     public Source() {
     }
@@ -28,11 +39,38 @@ public class Source {
         this.name = name;
     }
 
+    public String getSourceTitle() {
+        return mSourceTitle;
+    }
+
+    public void setSourceTitle(String mSourceTitle) {
+        this.mSourceTitle = mSourceTitle;
+    }
+
+    public int getImageResourceID() {
+        return mImageResourceID;
+    }
+
+    public void setImageResourceID(int mImageResourceID) {
+        this.mImageResourceID = mImageResourceID;
+    }
+
+    public boolean isToggleStatus() {
+        return mToggleStatus;
+    }
+
+    public void setToggleStatus(boolean mToggleStatus) {
+        this.mToggleStatus = mToggleStatus;
+    }
+
     @Override
     public String toString() {
-        return "Sources{" +
+        return "Source{" +
                 "id='" + id + '\'' +
                 ", name='" + name + '\'' +
+                ", mSourceTitle='" + mSourceTitle + '\'' +
+                ", mImageResourceID=" + mImageResourceID +
+                ", mToggleStatus=" + mToggleStatus +
                 '}';
     }
 }
