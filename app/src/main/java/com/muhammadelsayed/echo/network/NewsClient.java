b4466bd789b583fff31ecd646241ee5c36937626
mo15hammed <mo15hammed@hotmail.com>
2018-07-20 04:29:42 +0200

    adding more retrofit interfaces and their documentation

--- app/src/main/java/com/muhammadelsayed/echo/network/NewsClient.java
+++ app/src/main/java/com/muhammadelsayed/echo/network/NewsClient.java
@@ -10,7 +10,57 @@ import retrofit2.http.QueryMap;
 
 public interface NewsClient {
 
+
+    /**
+     * Retrieves the top headline news
+     * @param options the Query Strings that can be {
+     *
+     *     country  = The 2-letter ISO 3166-1 code of the country you want to get headlines for.
+     *
+     *     category = The category you want to get headlines for.
+     *                { business, entertainment, general, health, science, sports, technology }
+     *
+     *     q        = Keywords or phrases to search for.
+     *
+     *     sources  = A comma-seperated string of identifiers (maximum 20)
+     *               for the news sources or blogs you want headlines from.
+     *
+     *     pageSize = The number of results to return per page.
+     *                20 is the default, 100 is the maximum.
+     *
+     *     apiKey   = Your API key. (REQUIRED)
+     *
+     * }
+     *
+     * @return an object containing an array of the top headline news
+     */
     @GET("top-headlines")
-        Call<ResultArticles> getTopHeadLines(@QueryMap Map<String, String> options);
+    Call<ResultArticles> getTopHeadLines(@QueryMap Map<String, String> options);
+
+    /**
+     * Retrieves all articles that matches the options
+     * @param options the Query Strings that can be {
+     *
+     *     q        = Keywords or phrases to search for.
+     *
+     *     sources  = A comma-seperated string of identifiers (maximum 20)
+     *               for the news sources or blogs you want headlines from.
+     *
+     *     from     = A date and optional time for the oldest article allowed.
+     *
+     *     to       = A date and optional time for the newest article allowed.
+     *
+     *     language = he 2-letter ISO-639-1 code of the language you want to get headlines for.
+     *
+     *     pageSize = The number of results to return per page.
+     *                20 is the default, 100 is the maximum.
+     *
+     *     apiKey   = Your API key. (REQUIRED)
+     *
+     * }
+     * @return an object containing an array of all articles that matches the options
+     */
+    @GET("everything")
+    Call<ResultArticles> getArticles(@QueryMap Map<String, String> options);
 
 }
