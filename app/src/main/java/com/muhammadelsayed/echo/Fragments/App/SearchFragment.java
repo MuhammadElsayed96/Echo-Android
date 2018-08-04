01ed251b75776f3a0b5e677ade567c50c22292e9
MuhammadElsayed96 <muhammadelsayed96@yahoo.com>
2018-08-04 03:29:03 +0200

    Enable search functionality.

--- app/src/main/java/com/muhammadelsayed/echo/Fragments/App/SearchFragment.java
+++ app/src/main/java/com/muhammadelsayed/echo/Fragments/App/SearchFragment.java
@@ -2,15 +2,33 @@ package com.muhammadelsayed.echo.Fragments.App;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.muhammadelsayed.echo.Adapters.NewsAdapter;
 import com.muhammadelsayed.echo.R;
+import com.muhammadelsayed.echo.Utils;
+import com.muhammadelsayed.echo.model.Article;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SearchFragment extends Fragment {
     private static final String TAG = "SearchFragment";
+    private NewsAdapter mSearchNewsAdapter;
+    private RecyclerView mSearchRecycler;
+    private TextView mNoResults;
+    private SearchView mSearchView;
+    private List<Article> mSearchArticleList = new ArrayList<>();
 
     public SearchFragment() {
         // Required empty public constructor
@@ -32,7 +50,101 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         Log.wtf(TAG, "onCreateView() has been instantiated");
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_search, container, false);
+        mSearchRecycler = rootView.findViewById(R.id.search_recycler);
+        LinearLayoutManager mLinearLayoutManager =
+                new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        mSearchRecycler.setLayoutManager(mLinearLayoutManager);
+        mSearchRecycler.setDrawingCacheEnabled(true);
+        mSearchRecycler.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
+        mSearchRecycler.setItemViewCacheSize(100);
+        mSearchView = rootView.findViewById(R.id.search_view);
+        mNoResults = rootView.findViewById(R.id.no_results);
+        mSearchView = rootView.findViewById(R.id.search_view);
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //Log.e("onQueryTextChange", "called");
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                loadSearchData(mSearchView.getQuery().toString());
+
+                // Do your task here
+
+                return false;
+            }
+
+        });
+//        mSearchView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+//            @Override
+//            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+//                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+//                    loadSearchData(mSearchView.getText().toString());
+//                    return true;
+//                }
+//                return false;
+//            }
+//        });
+        return rootView;
     }
 
+    private void loadSearchData(String query) {
+        Log.wtf(TAG, "loadSearchData() has been instantiated");
+        mSearchView.clearFocus();
+        if (!TextUtils.isEmpty(query)) {
+            if (mSearchNewsAdapter != null)
+                mSearchNewsAdapter = null;
+            Map<String, Object> options = new HashMap<>();
+            options.put("q", query);
+            options.put("order-by", "newest");
+            options.put("show-tags", "contributor");
+            options.put("show-fields", "thumbnail,showInRelatedContent,shortUrl");
+            options.put("page", 1);
+            options.put("page-size", 20);
+            options.put("api-key", "c8133e91-2b02-42b7-9cc8-88ca8d73998a");
+            Utils.getSearchResults(options, new Utils.retrofitCallback() {
+                @Override
+                public void onSuccess(List<Article> Article) {
+                    Log.wtf(TAG, "onSuccess: SEARCH ARTICLES = " + Article);
+                    mSearchArticleList = Article;
+                    mSearchNewsAdapter = new NewsAdapter(getContext(), mSearchArticleList);
+                    mSearchRecycler.setAdapter(mSearchNewsAdapter);
+                }
+
+                @Override
+                public void onFailure(Throwable t) {
+                    Log.wtf(TAG, "onSuccess: SEARCH ARTICLES Failure -> " + t.getMessage());
+                }
+            });
+        } else {
+            mSearchRecycler.setVisibility(View.GONE);
+            mNoResults.setVisibility(View.VISIBLE);
+        }
+        mSearchRecycler.setAdapter(mSearchNewsAdapter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.wtf(TAG, "onPause() has been instantiated");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.wtf(TAG, "onStart() has been instantiated");
+        if (mSearchNewsAdapter != null)
+            mSearchRecycler.setAdapter(mSearchNewsAdapter);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.wtf(TAG, "onStop() has been instantiated");
+    }
 }
--- app/src/main/java/com/muhammadelsayed/echo/Utils.java
+++ app/src/main/java/com/muhammadelsayed/echo/Utils.java
@@ -46,7 +46,35 @@ public class Utils {
                 callback.onFailure(t);
             }
         });
+    }
 
+    public static void getSearchResults(Map<String, Object> options,
+                                        final retrofitCallback callback) {
+
+        NewsClient service = RetrofitClientInstance.getRetrofitInstance().create(NewsClient.class);
+        Call<Retrosponse> call = service.search(options);
+        Log.wtf(TAG, call.request().url().toString());
+        call.enqueue(new Callback<Retrosponse>() {
+            @Override
+            public void onResponse(Call<Retrosponse> call, Response<Retrosponse> response) {
+                if (response.body() != null) {
+                    if (response.body().getResponse().getStatus().equals("ok")) {
+
+                        Log.d(TAG, "onResponse: " + response.body());
+                        List<Article> articles =
+                                new ArrayList<>(Arrays.asList(response.body().getResponse().getArticles()));
+                        callback.onSuccess(articles);
+
+
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Retrosponse> call, Throwable t) {
+                callback.onFailure(t);
+            }
+        });
     }
 
     public interface retrofitCallback {
